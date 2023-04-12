7440113eedc7f453f8d4289b509f1a96e925004e
Davi Petris <davi.petris@oracle.com>
2023-04-12 19:04:13 -0300

    prime factors

---
+++ prime-factors/prime_factors.go
@@ -0,0 +1,25 @@
+package prime
+
+import (
+	"math"
+)
+
+func Factors(n int64) []int64 {
+	rest := n
+	limit := int64(math.Sqrt(float64(n)))
+	if limit < n {
+		limit = n
+	}
+	primes := []int64{}
+	for c := int64(2); c <= limit; c++ {
+		if rest <= 1 {
+			break
+		}
+		if rest%c == 0 {
+			rest /= c
+			primes = append(primes, c)
+			c--
+		}
+	}
+	return primes
+}
